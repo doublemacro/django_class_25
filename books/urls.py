7d52df571d8037e73c3a20c4a4ec05c87d1f7ecf
antochi <adrian.constantin.antochi@gmail.com>
2025-07-17 20:55:57 +0300

    started with django templates

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,6 @@ from books import views
 
 urlpatterns = [
     path('hello', views.hello, name='hello'),
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
+fruits = ['Apple', 'Pineapple', 'Durian', 'Passion fruit', 'Banana', 'Pear']
+
 def hello(request):
     return HttpResponse("Hello from our Books app!")
+
+def home(request):
+    # Django -> Jinja(Template Engine)
+
+    context = {
+        'username': 'Alice',
+        'logged_in': True,
+        'current_time': datetime.now(),
+        'fruits': fruits
+    }
+
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html')
